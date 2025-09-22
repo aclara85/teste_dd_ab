0ece72cc8ad4b93b096fd14d8f89e80501ed04c0
Antônia Clara <antonia.clara08@alu.ufc.br>
2025-09-22 16:21:18 -0300

    fazendo atividade da toalha

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,18 +1,39 @@
 class Towel: 
     def __init__(self, color:str, size: str): #construtor
-        self.color="red"
-        self.size="p"
-        self.wetness="0"
+        self.color: str= color
+        self.size: str = size
+        self.wetness: int = 0
 
-        def __str__(self):
-            return f"color:"
+    def dry(self, amount: int)-> None: 
+            self.wetness += amount
+            if self.wetness >= self.getMaxWetness():
+                print("Toalha encharcada")
+                self.wetness = self.getMaxWetness()
 
-towel = Towel("green", "G")
-toalha = Towel("red", "G")
-outra = Towel("green", "G")
-toalha = Towel("red", "G")
-print(towel.color)
-towel.color="white"
-print(towel.color)
-print(towel.size)
-print(towel.wetness)
+    def wringOut(self)-> None:
+            self.wetness = 0
+    
+
+    def getMaxWetness(self) -> int:
+            if self.size == "p":
+                return 10
+            if self .size == "m":
+                return 20       
+            if self.size == "g":
+                return 30
+            return 0 
+        
+    def __str__(self) -> str:
+            return f"Cor: {self.color}, Tam:{self.size}, Umidade:{self.wetness}"
+        
+
+doguito= Towel("velha", "m")   
+print(doguito)
+doguito.dry(3)
+print(doguito)
+doguito.dry(15)
+doguito.dry(10)
+print(doguito)
+        
+  
+
