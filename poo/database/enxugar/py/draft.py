bae43155fdae58b7746b49e0e5de7ab8a005b910
Antônia Clara <antonia.clara08@alu.ufc.br>
2025-10-05 13:40:19 -0300

    atvidades do bloco b resolvidas

--- poo/database/enxugar/py/draft.py
+++ poo/database/enxugar/py/draft.py
@@ -1,58 +1,59 @@
-class Towel: 
-    def __init__(self, color:str, size: str): #construtor
-        self.color: str= color
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color: str = color
         self.size: str = size
         self.wetness: int = 0
 
-    def dry(self, amount: int)-> None: 
-            self.wetness += amount
-            if self.wetness >= self.getMaxWetness():
-                print("Toalha encharcada")
-                self.wetness = self.getMaxWetness()
+    def dry(self, amount: int) -> None:
+        self.wetness += amount
+        if self.wetness >= self.isMaxWetness():
+            self.wetness = self.isMaxWetness()
+            print("toalha encharcada")
 
-    def wringOut(self)-> None:
-            self.wetness = 0
-    
+    def isDry(self):
+        return self.wetness == 0
 
-    def getMaxWetness(self) -> int:
-            if self.size == "p":
-                return 10
-            if self .size == "m":
-                return 20       
-            if self.size == "g":
-                return 30
-            return 0 
-        
+    def wringOut(self):
+        self.wetness = 0
+
+    def isMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
+
     def __str__(self) -> str:
-            return f"Cor: {self.color}, Tamanho:{self.size}, Umidade:{self.wetness}"
-        
+        return f"Cor: {self.color}, Tamanho: {self.size}, Umidade: {self.wetness}"
+
 
 def main():
-   toalha = Towel("azul", "m") #objeto manipulado
-   while True:
-        line:str = input()
-        args:list[str] = line.split(" ")
+    towel: Towel = Towel("", "")
+    while True:
+
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
 
         if args[0] == "end":
-            break   
+            break
         elif args[0] == "criar":
-             color = args[1]
-             size = args[2]    
-             toalha = Towel(color, size)
-        elif args[0] == "mostrar":
-            print(toalha)
-        
+            color: str = args[1]
+            size: str = args[2]
+            towel = Towel(color, size)
         elif args[0] == "seca":
-            print("sim" if toalha.dry() else "não")
-        elif args[0] == "dry": #amout
-            amount:int = int(args[1])
-            toalha.dry(amount)
+            print("sim" if towel.isDry() else "nao")
+        elif args[0] == "torcer":
+            towel.wringOut()
+        elif args[0] == "enxugar":
+            amount: int = int(args[1])
+            towel.dry(amount)
+        elif args[0] == "mostrar":
+            print(towel)
         else:
-            print("fail: Comando inválido")    
-        
-  
-main()
+            print("fail: comando não encontrado")
 
-def main():
-     toalha = Towel 
-     main()
+
+main()
